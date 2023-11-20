9d28294c3fbf09e2ed1cf7c54ee8b8d5d4b88708
mlumoura <mlumoura@gmail.com>
2023-11-20 17:57:00 -0300

    fix app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ headers = {
 
 app = Flask(__name__)  # Inicializa a aplicação
 
+
 @app.route('/')  # Nova rota
 def main():
     resultado = None
@@ -15,34 +16,35 @@ def main():
     cotacao = 0
     valor_convertido = 0
 
-
     def cotacao(moeda):
-        moedas = ['dolar', 'euro', 'peso argentino','yuan']
+        # Lista das moedas disponíveis para apurar cotação
+        moedas = ['dolar', 'euro', 'koruna', 'peso argentino', 'peso chileno', 'pound sterling', 'yuan', 'yen']
         if moeda in moedas:
-            page = requests.get(f"""https://www.google.com/search?q={moeda}&oq={moeda}&sclient=gws-wiz-serp&sourceid=chrome&ie=UTF-8""",headers=headers)
-            soup = BeautifulSoup(page.content, 'html.parser')  
+            page = requests.get(
+                f"""https://www.google.com/search?q={moeda}&oq={moeda}&sclient=gws-wiz-serp&sourceid=chrome&ie=UTF-8""",
+                headers=headers)
+            soup = BeautifulSoup(page.content, 'html.parser')
 
             valor = soup.find_all("span", class_="DFlfde SwHCTb")[0]
             cotacao = valor.text
             cotacao = cotacao.replace(',', '.')
             cotacao = float(cotacao)
             valor_convertido = round((real / cotacao), 3)
-            resultado = f"""O valor R${real}0 convertido é: {valor_convertido} {moeda} (Cotação ={cotacao})"""  
+            resultado = f"""O valor R${real} convertido é: {valor_convertido} {moeda} (Cotação ={cotacao})"""
 
             return resultado
         else:
             resultado = "Esta moeda é inválida ou não está disponível para conversão"
             return resultado
 
-        
     real = request.args.get('real')
     moeda = request.args.get('moeda')
-    
+
     if real and moeda:
         real = float(real)
         moeda = moeda.lower()
 
-        resultado = cotacao(moeda)      
+        resultado = cotacao(moeda)
 
     return render_template('index.html', resultado=resultado)
 
@@ -50,13 +52,4 @@ def main():
 if __name__ == '__main__':
     app.run(debug=True)  # Executa a aplicação
 
-
-
-
-
-
-
-
-
-
-#gunicorn -w 3 app:app
+# gunicorn -w 3 app:app
